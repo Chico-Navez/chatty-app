bac562ef2cdd4207593abefb0f71aa0e2f31a523
Chico-Navez <ngiannulli@gmail.com>
2017-10-19 12:31:59 -0700

    showing text on page

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -7,11 +7,16 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = app => {
+
+	app.get('/', (req, res) => {
+		res.send('<h1>The App Worked!</h1>');
+	});
+
 	app.post('/signin', requireSignin, Authentication.signin);
 	app.post('/signup', Authentication.signup);
 	app.get('/chat-lobby', requireAuth, (req, res) => {
 		console.log('/chat-lobby', req.user);
 		res.send({temp: 'Say Something'});
-	})
+	});
 
 }
